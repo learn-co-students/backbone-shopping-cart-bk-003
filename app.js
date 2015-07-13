fdc43683f1d000502102e0290e83dd95eaa9d23d
Katie Hoffman <ktahoffman@gmail.com>
2015-07-13 12:17:59 -0400

    remove passing code

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 app = {};
+
 function bootstrap() {
-  // instantiate your view object and append it's HTML to the DOM.
-  var user = new User();
-  app.formView = new FormView({model : user});
-  $("body").append(app.formView.el);
+  // instantiate a new user object and save it as a variable here
+
+  // instantiate your view object here:
+  //   * it should take one argument, an object
+  //      - the object should have a property called "model"
+  //        which should point to the new instance of the user  
+
+  // append your view object's HTML to the DOM here
 }
+
 bootstrap();
--- views/main.js
+++ views/main.js
@@ -1,16 +1,19 @@
 FormView = Backbone.View.extend({
   events : {
-    "submit form" : "submitHandler"
+    // you'll want to add a submit event to the form
+    // it should trigger the submitHandler function
   },
   initialize : function() {
-    this.render();
+    // call on this instance's render function here
   },
   render : function() {
-    this.$el.append('<form><label for="name"></label><input id="name" type="text" name="name"><input id= "mySubmit" type="submit" value="submit"></form>')
-    return this;
+    // here is the html you want to append to $el
+    // <form><label for="name"></label><input id="name" type="text" name="name"><input id= "mySubmit" type="submit" value="submit"></form>
+    // remember to return the instance of the FormView
   },
-  submitHandler : function(e) {
-    e.preventDefault();
-    this.model.set("name", $("#name").val());
+  submitHandler : function() {
+    // prevent the default behavior of the form submitting
+    // set the model's name to have a value of whatever
+    // text is in the input field with an id of name here
   }
 })
