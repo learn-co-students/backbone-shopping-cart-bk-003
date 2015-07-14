0d8bcbc3ef4c90d615def4115ad2c71b23ed9880
Katie Hoffman <ktahoffman@gmail.com>
2015-07-14 14:38:14 -0400

    require closure for tests, update specs

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -24,6 +24,11 @@ describe(".Cart", function() {
   });
 
   describe("#addShirtToCart", function() {
+    var cart;
+    beforeEach(function() {
+      cart = new Cart;
+    });
+
     it("accepts two parameters, style and size, and adds them as an object to items", function() {
       cart.addShirtToCart("locals", "large");
       var items = cart.get("items");
@@ -31,22 +36,55 @@ describe(".Cart", function() {
       var shirt = items[0];
       expect(shirt.style).toEqual("locals");
       expect(shirt.size).toEqual("large");
+    }); 
+
+    it("updates the subTotal (each shirt is 24.99)", function() {
+      cart.addShirtToCart("locals", "large");
+      expect(cart.get("items").length).toEqual(1);
+      expect(cart.get("subTotal")).toEqual(24.99);
+      cart.addShirtToCart("dino", "medium");
+      expect(cart.get("items").length).toEqual(2);
+      expect(cart.get("subTotal")).toEqual(49.98);
+    });
+
+    it("updates the shipping cost (standard 7.29 charge for one or more shirts)", function() {
+      cart.addShirtToCart("locals", "large");
+      expect(cart.get("shipping")).toEqual(7.29);
+      cart.addShirtToCart("dino", "medium");
+      expect(cart.get("shipping")).toEqual(7.29);
+    });
+
+    it("updates cost of taxes with a sales tax of 8.875% rounded *up* to the nearest cent", function() {
+      cart.addShirtToCart("locals", "large");
+      expect(cart.get("taxes")).toEqual(2.22);
+      cart.addShirtToCart("dino", "medium");
+      expect(cart.get("taxes")).toEqual(4.44);
+    });
+
+    it("updates total cost which is the sum of subTotal, taxes, and shipping rounded to the nearest cent", function() {
+      cart.addShirtToCart("locals", "large");
+      expect(cart.get("total")).toEqual(34.50);
+      cart.addShirtToCart("dino", "medium");
+      expect(cart.get("total")).toEqual(61.71);
     });  
   });
 });
 
 describe("#lonelyDino", function() {
-  it("instantiates a new cart, passes the cart to the view, attaches the view to the app", function() {
+  beforeEach(function() {
+    var app = {};
     lonelyDino();
+  });
+  
+  it("instantiates a new cart, passes the cart to the view, attaches the view to the app", function() {
     expect(typeof app).toEqual("object");
     expect(typeof app.formView).toEqual("object");
     expect(typeof app.formView.model).toEqual("object");
-    expect(typeof app.formView.model.get("items")).toEqual([]);
-    expect(typeof app.formView.model.get("subTotal")).toEqual(0.00);
+    expect(app.formView.model.get("items")).toEqual([]);
+    expect(app.formView.model.get("subTotal")).toEqual(0.00);
   });
   
   it("appends the html to the body of the page", function() {
-    lonelyDino();
     var html = '<div class="container"><div class="jumbotron"><div class="row"><div class="col-md-4"><img src="public/img/logos/logo.png" /></div><div class="col-md-8"><h1>Lonely Dinosaur Designs</h1></div></div></div><div class="row"><div class="col-md-8 col-md-offset-2"><h2>Select a T-Shirt</h2><form id="shirt-form"><div id="shirt-style"><p><strong>Style:</strong></p><label><img src="public/img/shirts/dino.jpg" /><br><input type="radio" name="style" value="dino">\nAll My Friends Are Dead</label><label><img src="public/img/shirts/down.jpg" /><br><input type="radio" name="style" value="down">\nI\'m Down</label><label><img src="public/img/shirts/locals.jpg" /><br><input type="radio" name="style" value="locals">\nEat Locals</label></div><div id="shirt-size"><label id="size">Size:</label><br><select name="size"><option value="x-small">X-Small</option><option value="small">Small</option><option value="medium">Medium</option><option value="large">Large</option><option value="x-large">X-Large</option></select></div><div id="button-container"><button type="submit">Purchase</button></div></form></div></div></div>';
     expect($("body")).toContainHtml(html);
   });
@@ -67,10 +105,8 @@ describe("Integration Test", function() {
     expect(cart.get("total")).toEqual(34.50);
     
     var items = cart.get("items");
-    expect(items).toBeArray();
-    
     var tShirt = items[0];
-    expect(tShirt).toBeObject();
+    expect(typeof items[0]).toEqual("object");
     expect(tShirt.style).toEqual("down");
     expect(tShirt.size).toEqual("medium");
   });
