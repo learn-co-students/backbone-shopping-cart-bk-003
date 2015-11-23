d092d113dc29bdd267bc7302168d60418b5c3095
JaimieWalker <jaimie.walker90@gmail.com>
2015-11-23 14:40:45 -0500

    This was interesting

--- public/js/models/cart.js
+++ public/js/models/cart.js
@@ -1,7 +1,10 @@
-Cart = Backbone.Model.extend({
-  defaults: function() { 
-    return {
-
+Cart = Backbone.Model.extend({  
+ defaults: function() {      return {
+    items : [],
+    subTotal : 0,
+    shipping : 0,
+    taxes : 0,
+    total : 0
       // here you'll put the default properties you'll want to use
       // for instance, if you want to default the number of wheels of the cart
       // and the cart's name (not that a cart having a name makes any sense) 
@@ -12,6 +15,23 @@ Cart = Backbone.Model.extend({
     };
   },
   addShirtToCart: function(style, size) {
+    shirt = {};
+    shirt["style"] = style;
+    shirt["size"] = size;
+    
+    cartItems = this.get("items");
+    cartItems.push(shirt);
+    this.set("items", cartItems);
+    oldTotal = this.get("subTotal");
+    oldTotal += 24.99;
+    this.set("subTotal", oldTotal);
+    this.set("shipping", 7.29);
+    var salesTax = 0.08875;
+    var tax = this.get("subTotal") * salesTax;
+    this.set("taxes", parseFloat(tax.toFixed(2)));
+    total = this.get("shipping") + this.get("taxes") + this.get("subTotal")
+    
+    this.set("total",parseFloat(total.toFixed(2)) )
     // this function takes two params
     // it should make a "shirt" object, add it to items
     // it should then update the subTotal, shipping cost, taxes
