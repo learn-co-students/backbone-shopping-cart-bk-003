3b1041631f21c951d182c1684ed2238d5894d6fb
Lawrence Chong <mail.to.lawrence@gmail.com>
2015-11-23 19:36:38 -0500

    fin

--- public/js/models/cart.js
+++ public/js/models/cart.js
@@ -1,7 +1,11 @@
 Cart = Backbone.Model.extend({
   defaults: function() { 
     return {
-
+      items : [],
+      subTotal : 0,
+      taxes : 0,
+      shipping : 0,
+      total : 0
       // here you'll put the default properties you'll want to use
       // for instance, if you want to default the number of wheels of the cart
       // and the cart's name (not that a cart having a name makes any sense) 
@@ -14,7 +18,35 @@ Cart = Backbone.Model.extend({
   addShirtToCart: function(style, size) {
     // this function takes two params
     // it should make a "shirt" object, add it to items
+    this.addShirtToItems(size, style);
+    this.updateSubtotal();
+    this.updateShipping();
+    this.updateTaxes();
+    this.updateTotal();
     // it should then update the subTotal, shipping cost, taxes
+    
     // it should then use these updated values to update the total
+  },
+  addShirtToItems: function (size, style) {
+    var temp_arr = this.get('items');
+    var shirt = {'size' : size, 'style' : style};
+    temp_arr.push(shirt);
+    // this.set('items', temp_arr);
+  },
+  updateSubtotal: function () {
+    // each shirt is $24.99
+    // get current total and add to it
+    this.set('subTotal', (this.get('subTotal') + 24.99) );
+  },
+  updateShipping: function () {
+    if (this.get('items').length >= 1) {
+      this.set('shipping', 7.29);
+    }; 
+  },
+  updateTaxes: function (){
+    this.set('taxes', (Math.ceil(this.get('subTotal')*.08875*100 ))/100 );
+  },
+  updateTotal: function (){
+    this.set('total', Math.ceil((this.get('subTotal') + this.get('taxes') + this.get('shipping'))*100)/100 )
   }
 });
--- public/js/views/main.js
+++ public/js/views/main.js
@@ -2,30 +2,42 @@ FormView = Backbone.View.extend({
   events : {
     // you'll want to add a click event to the button with an id of add-to-cart
     // clicking this button should trigger the addToCart function
+    'submit' : "addToCart"
   },
   initialize : function() {
     // call on this instance's render function here
+    this.render();
   },
   render : function() {
     // here is the html you want to append to $el:
-    // <div class="container"><div class="jumbotron"><div class="row"><div class="col-md-4"><img src="public/img/logos/logo.png" /></div><div class="col-md-8"><h1>Lonely Dinosaur Designs</h1></div></div></div><div class="row"><div class="col-md-8 col-md-offset-2"><h2>Select a T-Shirt</h2><form id="shirt-form"><div id="shirt-style"><p><strong>Style:</strong></p><label><img src="public/img/shirts/dino.jpg" /><br><input type="radio" name="style" value="dino">\nAll My Friends Are Dead</label><label><img src="public/img/shirts/down.jpg" /><br><input type="radio" name="style" value="down">\nI\'m Down</label><label><img src="public/img/shirts/locals.jpg" /><br><input type="radio" name="style" value="locals">\nEat Locals</label></div><div id="shirt-size"><label id="size">Size:</label><br><select name="size"><option value="x-small">X-Small</option><option value="small">Small</option><option value="medium">Medium</option><option value="large">Large</option><option value="x-large">X-Large</option></select></div><div id="button-container"><button type="submit">Purchase</button></div></form></div></div></div>
+    var html  = '<div class="container"><div class="jumbotron"><div class="row"><div class="col-md-4"><img src="public/img/logos/logo.png" /></div><div class="col-md-8"><h1>Lonely Dinosaur Designs</h1></div></div></div><div class="row"><div class="col-md-8 col-md-offset-2"><h2>Select a T-Shirt</h2><form id="shirt-form"><div id="shirt-style"><p><strong>Style:</strong></p><label><img src="public/img/shirts/dino.jpg" /><br><input type="radio" name="style" value="dino">\nAll My Friends Are Dead</label><label><img src="public/img/shirts/down.jpg" /><br><input type="radio" name="style" value="down">\nI\'m Down</label><label><img src="public/img/shirts/locals.jpg" /><br><input type="radio" name="style" value="locals">\nEat Locals</label></div><div id="shirt-size"><label id="size">Size:</label><br><select name="size"><option value="x-small">X-Small</option><option value="small">Small</option><option value="medium">Medium</option><option value="large">Large</option><option value="x-large">X-Large</option></select></div><div id="button-container"><button type="submit">Purchase</button></div></form></div></div></div>'
 
     // remember to return the instance of the FormView
+    this.$el.append(html);
+    return this;
   },
-  addToCart : function() {
-    // make sure this function accepts the event parameter
+  addToCart : function(e) {
+  //   // make sure this function accepts the event parameter
 
-    // prevent the default behavior (submission) of the form 
-    // (call preventDefault on the event) 
+  //   // prevent the default behavior (submission) of the form 
+  //   // (call preventDefault on the event) 
+     e.preventDefault();
 
-    // call the serialize() function on the form 
 
-    // save the radio button value as style
+  //   // call the serialize() function on the form 
+    // var params = $(#shirt-form).serialize();
 
-    // save the dropdown value as size
+  //   // save the radio button value as style
+     // var style = params.matching
+     var params = $("#shirt-form").serialize().split(/[=&]/);
+     var size = params[3];
+     var style = params[1];
 
-    // call the function addShirtToCart on the model, be sure to pass it two parameters:
-    //   1. the style
-    //   2. the size
+  //   // save the dropdown value as size]
+
+  //   // call the function addShirtToCart on the model, be sure to pass it two parameters:
+  //   //   1. the style
+  //   //   2. the size
+        this.model.addShirtToCart(style,size);
   }
 });
