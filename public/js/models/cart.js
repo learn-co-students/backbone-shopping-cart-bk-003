e86c99d4e88980cbf40f4b4d11c616c398e5d852
bmesa620 <bmesa620@gmail.com>
2015-11-24 11:25:36 -0500

    woohoo

--- public/js/models/cart.js
+++ public/js/models/cart.js
@@ -9,6 +9,11 @@ Cart = Backbone.Model.extend({
 
       // wheelNum: 4,
       // name: "McCarty Cart Cart Jr."
+      items: [],
+      subTotal: 0,
+      total: 0,
+      shipping: 0,
+      taxes: 0
     };
   },
   addShirtToCart: function(style, size) {
@@ -16,5 +21,13 @@ Cart = Backbone.Model.extend({
     // it should make a "shirt" object, add it to items
     // it should then update the subTotal, shipping cost, taxes
     // it should then use these updated values to update the total
+    var shirt = {style: style, size: size};
+    this.get('items').push(shirt);
+    var updatedItems = this.get('items');
+    this.set('items', updatedItems);
+    this.set('subTotal', this.get('items').length * 24.99);
+    this.set('shipping', 7.29);
+    this.set('taxes', Number(((this.get("subTotal") * 8.875)/100).toFixed(2)));
+    this.set('total', Number((this.get("subTotal") + this.get("shipping") + this.get("taxes")).toFixed(2)));
   }
 });
